c6ece7f3543770916545e66873c221edfb307ce5
huamaniERICK <erickhuamaniccahuay10@gmail.com>
2024-11-08 21:06:31 -0500

    agrego una imagen a assets

--- app_FutbolitoOnline_landingpage/componentes/seccion.py
+++ app_FutbolitoOnline_landingpage/componentes/seccion.py
@@ -5,9 +5,11 @@ def seccion()->rx.Component:
         rx.text(" Diseña tu torneo ideal ",size="9",color=rx.color("cyan",8)),
         rx.text("¡Las reglas lo pones tú!",size="9",color=rx.color("red",10))),
 
+    
+
       rx.container(
         rx.text("-Ahorra tiempo y esfuerzo en la organizacion de torneos ."),
-        rx.text("-Crea torneos memorables y exitosos deacuerdo a tu estilo."),
+        rx.text("-Crea torneos memorables y exitosos deacuerdo a tu estilo ."),
         rx.text("-Manten a tus participantes informados con notificaciones."),
         rx.link(rx.button("REGISTRATE",margin_top="4em",color_scheme="cyan"),href="https://forms.gle/Apc6aXEWNma79Nfs7",is_external=True),
         margin_top="1em"
